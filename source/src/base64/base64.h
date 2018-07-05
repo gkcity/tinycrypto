75ac3a071aa0cb64df0a026e6e34aff13bb53fd4
ouyang <jxfengzi@gmail.com.cn>
2018-07-05 20:20:30 +0800

    修复在vs2017里的编译错误

--- source/src/base64/base64.h
+++ source/src/base64/base64.h
@@ -16,16 +16,27 @@
 #define __BASE64_H__
 
 #include <tiny_base.h>
+#include <tiny_lor.h>
+#include <common/tiny_crypto_api.h>
 
 TINY_BEGIN_DECLS
 
 
+TINY_LOR
+TINY_CRYPTO_API
 uint32_t base64_decode_out_length(const char *string);
+
+TINY_LOR
+TINY_CRYPTO_API
 
 uint32_t base64_decode(const char *string, uint8_t *out);
 
+TINY_LOR
+TINY_CRYPTO_API
 uint32_t base64_encode_out_length(int bytesLength);
 
+TINY_LOR
+TINY_CRYPTO_API
 uint32_t base64_encode(const uint8_t *bytes, int length, char *out);
 
 
